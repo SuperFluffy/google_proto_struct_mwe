5bc9ccd5c5a2cb5e92ec388bee4fe6cba762279c
Janis Goldschmidt <github@aberrat.io>
2022-01-25 11:16:32 +0100

    Remove prost config

--- build.rs
+++ build.rs
@@ -4,11 +4,6 @@ fn main() -> io::Result<()> {
     let builder = tonic_build::configure()
         .compile_well_known_types(true);
 
-    let mut prost_config = prost_build::Config::new();
-    prost_config
-        .compile_well_known_types()
-        .btree_map(&["."]);
-
-    builder.compile_with_config(prost_config, &["./protos/myproto.proto",], &["./protos"])?;
+    builder.compile(&["./protos/myproto.proto",], &["./protos"])?;
     Ok(())
 }
