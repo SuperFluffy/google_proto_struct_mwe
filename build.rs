5c6127d7b2c7bc970d0d50e9d9f34bf260542f03
Janis Goldschmidt <github@aberrat.io>
2022-01-25 11:22:45 +0100

    Remove prost-build, include proto structs inline

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() -> io::Result<()> {
-    let builder = tonic_build::configure()
-        .compile_well_known_types(true);
-
-    builder.compile(&["./protos/myproto.proto",], &["./protos"])?;
-    Ok(())
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,40 @@
-#![allow(
-    unreachable_pub,
-    clippy::all,
-    clippy::doc_markdown,
-    clippy::similar_names,
-    clippy::wildcard_imports
-)]
-
-#[allow(clippy::all)]
-pub mod mypackage {
-    include!(concat!(env!("OUT_DIR"), "/mypackage.rs"));
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Struct {
+    #[prost(map = "string, message", tag = "1")]
+    pub fields: ::std::collections::HashMap<::prost::alloc::string::String, Value>,
 }
 
-pub mod google {
-    pub mod protobuf {
-        include!(concat!(env!("OUT_DIR"), "/google.protobuf.rs"));
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Value {
+    #[prost(oneof = "value::Kind", tags = "1, 2, 3, 4, 5, 6")]
+    pub kind: ::core::option::Option<value::Kind>,
+}
+pub mod value {
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Kind {
+        #[prost(enumeration = "super::NullValue", tag = "1")]
+        NullValue(i32),
+        #[prost(double, tag = "2")]
+        NumberValue(f64),
+        #[prost(string, tag = "3")]
+        StringValue(::prost::alloc::string::String),
+        #[prost(bool, tag = "4")]
+        BoolValue(bool),
+        #[prost(message, tag = "5")]
+        StructValue(super::Struct),
+        #[prost(message, tag = "6")]
+        ListValue(super::ListValue),
     }
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ListValue {
+    #[prost(message, repeated, tag = "1")]
+    pub values: ::prost::alloc::vec::Vec<Value>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum NullValue {
+    NullValue = 0,
 }
